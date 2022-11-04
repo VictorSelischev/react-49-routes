f274b5d96d60f05c0f423c5d32b92c9cb66398c9
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-04 18:33:53 +0200

    Routes & Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from "page/Home";
+import About from "page/About";
+import Products from "page/Products";
+import NotFound from 'page/NotFound';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +17,12 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
