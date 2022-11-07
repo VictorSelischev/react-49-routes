4b7b95f7bc8f574a88a89072080537b23e424368
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-07 18:54:11 +0200

    React.lazy( ) & React.Suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import { Mission } from './Mission';
+// import { Mission } from './Mission';
 import { Team } from './Team';
 import { Reviews } from './Reviews';
 
@@ -20,22 +21,25 @@ import { SharedLayout } from './SharedLayout/SharedLayout';
 // }`;
 
 export const App = () => {
+  const Mission = lazy(() => import('./Mission'));
 
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
+    <Suspense fallback={<div>Loading...</div>} >
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
 
-        <Route path="/about" element={<About />}>
-          <Route path="mission" element={<Mission />} />
-          <Route path="team" element={<Team />} />
-          <Route path="reviews" element={<Reviews />} />
+          <Route path="/about" element={<About />}>
+            <Route path="mission" element={<Mission />} />
+            <Route path="team" element={<Team />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
-
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 };
